a55f0cff294d00948d2eb97f4f5f9806c1766f5d
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-10-16 16:46:32 -0400

    added buttons and title

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,17 @@
-const test = document.querySelector('.testTitle').innerHTML
+const test = document.querySelector('.testTitle')
 
 console.log(test)
+
+
+const testBtn = document.querySelectorAll('.testBtn')
+
+testBtn.forEach((btn) => {
+    btn.addEventListener('click', () => {
+        console.log(btn)
+        test.classList.add('testColor')
+    })
+})
+
+// testBtn.addEventListener('click', () => {
+    
+// })
