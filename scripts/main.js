266360c83c21c93751e2a4e8efd1211be0d3efdc
Victor Solano <127169470+victorsolano-vs@users.noreply.github.com>
2024-10-20 14:50:01 -0400

    added functionality to button and add task to an array

--- scripts/main.js
+++ scripts/main.js
@@ -1,17 +1,59 @@
-const test = document.querySelector('.testTitle')
+const test = document.querySelector('.appTitle')
 
 console.log(test)
 
+const myTasks = ['watch football', 'wash dishes'];
 
-const testBtn = document.querySelectorAll('.testBtn')
+console.log(myTasks)
 
-testBtn.forEach((btn) => {
-    btn.addEventListener('click', () => {
-        console.log(btn)
-        test.classList.add('testColor')
-    })
+// setTimeout(() => {
+//     const inputValue = document.querySelector('.taskInput')
+//     myTasks.push(inputValue.value)
+//     console.log(inputValue.value)
+//     console.log(myTasks)
+// }, 10000)
+
+function addInput() {
+    // get input value from input tag
+    const inputValue = document.querySelector('.taskInput').value
+   
+    if(inputValue === ''){
+        alert('Please enter a task.')
+        return
+    } else {
+        // add value to array
+        myTasks.push(inputValue)
+
+        // reset the input
+        document.querySelector('.taskInput').value = ''
+
+        // re-render tasks html
+        renderTasks()
+        
+    }
+}
+
+const addTaskBtn = document.querySelector('.addTaskBtn')
+addTaskBtn.addEventListener('click', () => {
+    
+    addInput()
+    console.log(myTasks)
+    
 })
 
-// testBtn.addEventListener('click', () => {
-    
-// })
+
+function renderTasks() {
+    let taskCardsHTML = ''
+
+    myTasks.forEach((task) => {
+        taskCardsHTML += `
+        
+        <h1>${task}</h1>
+        `
+    })
+    console.log(taskCardsHTML)
+
+    document.querySelector('.tasksContainer').innerHTML = taskCardsHTML
+}
+
+//renderTasks()
